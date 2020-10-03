5177a8da3ca20e2e6755bbb7f6fa5e65f4ba4462
Ualter <ualter.junior@gmail.com>
2020-10-03 22:03:32 +0200

    .

--- get-my.sh
+++ get-my.sh
@@ -1,12 +1,16 @@
+#!/bin/bash
+
+rm -rf ~/.myfunctions/
 git clone -b rel https://github.com/ualter/mybashfunctions.git ~/.myfunctions
 echo "source ~/.myfunctions/.myfunctions.sh" >> ~/.bashrc
 echo "source ~/.myfunctions/.myfunctions.sh" >> ~/.zshrc
-sleep 2
-source ~/.zshrc
-sleep 1
 source ~/.bashrc
+sleep 2
 clear
-myf
 echo ""
-echo "Installed!"
+echo "----------------------"
+printf "\033[0;93mInstalled!\n"
+printf "\033[0;32mRun \033[0;94mmyf\033[0;32m to access help\033[0m\n"
+echo "----------------------"
 echo ""
+
