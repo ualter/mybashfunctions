c48ec12c2e4c7fcbff4f117e5e1fb51395d8933c
Ualter <ualter.junior@gmail.com>
2020-10-03 16:26:34 +0200

    .

--- get-my.sh
+++ get-my.sh
@@ -1,5 +1,6 @@
 git clone -b rel https://github.com/ualter/mybashfunctions.git ~/.myfunctions
 echo "source ~/.myfunctions/.myfunctions.sh" >> ~/.bashrc
+echo "source ~/.myfunctions/.myfunctions.sh" >> ~/.zshrc
 source ~/.myfunctions/.myfunctions.sh
 myf
 echo ""
