bf607a70bb73496239716bfb11d58517c556e886
Ualter <ualter.junior@gmail.com>
2020-10-03 16:30:12 +0200

    .

--- get-my.sh
+++ get-my.sh
@@ -2,6 +2,9 @@ git clone -b rel https://github.com/ualter/mybashfunctions.git ~/.myfunctions
 echo "source ~/.myfunctions/.myfunctions.sh" >> ~/.bashrc
 echo "source ~/.myfunctions/.myfunctions.sh" >> ~/.zshrc
 source ~/.myfunctions/.myfunctions.sh
+sleep 3
+source ~/.myfunctions/.myfunctions.sh
+clear
 myf
 echo ""
 echo "Installed!"
