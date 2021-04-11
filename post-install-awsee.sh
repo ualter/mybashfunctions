df49a8e1dd4cfdddd275366ba0edfd42742aeecf
Ualter Otoni Pereira <ualter@users.noreply.github.com>
2021-04-11 14:40:12 +0200

    Update post-install-awsee.sh

--- post-install-awsee.sh
+++ post-install-awsee.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 AWSEE_SCRIPT="$(python -c 'import os, sys; print(os.path.dirname(sys.executable))')/awsee"
 chmod +x $AWSEE_SCRIPT
-echo "alias awsee='source awsee'" >> ~/.bashrc 
+echo "alias awsee='source awsee'" >> ~/.bashrc
+alias awsee='source awsee'
+echo.
+echo. AWSee Ready!   
+echo.
+echo. Try: awsee -u  or awsee -h
+echo.
